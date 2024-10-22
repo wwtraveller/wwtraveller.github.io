76c4e92e9d9cc6e7995b0cd7b82ede0803b97190
Alena Shilimova <ashilimova@mail.ru>
2024-10-22 22:01:31 +0200

    initial commit, add some configuration files and update dependencies

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { About, Experience, Projects } from './sections'
+
+function App() {
+
+  return (
+    <>
+    <main className="max-w-7xl mx-auto relative">
+      {/* <Navbar /> */}
+      {/* <Hero /> */}
+      <About />
+      <Projects />
+      {/* <Clients /> */}
+      <Experience />
+      {/* <Contact /> */}
+      {/* <Footer /> */}
+    </main> 
+    </>
+  )
+}
+
+export default App
