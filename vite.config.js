2927970c83bae26fb58a55c515cc0ee9b6364b2f
Alena Shilimova <ashilimova@mail.ru>
2024-10-23 10:28:09 +0200

    update build

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
 import { defineConfig } from 'vite';
 import path from 'path';
-import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
-
-// Определяем __dirname для ES-модулей
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/wwtraveller.github.io',
-  plugins: [react(), TanStackRouterVite()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    base: '/wwtraveller.github.io',
+    plugins: [react(), TanStackRouterVite()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-  },
 });
